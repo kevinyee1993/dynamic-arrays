cbee21f095d848431535e6b57ce57e39251ec2a8
Kevin Yee <kevinyee1993@gmail.com>
2018-04-30 22:31:59 -0700

    Pass specs for dynamic_array.rb

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -5,36 +5,51 @@ class DynamicArray
 
   def initialize
     self.length = 0
+    self.store = []
+    self.capacity = 8
   end
 
   # O(1)
   def [](index)
     raise("index out of bounds") if index >= self.length
-
-
+    return store[index]
   end
 
   # O(1)
   def []=(index, value)
+    return nil if index > self.length
+    self.store[index] = value
   end
 
   # O(1)
   def pop
     raise("index out of bounds") if self.length == 0
+    self.store.pop
+    self.length -= 1
+
+
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    self.store.push(val)
+    self.length += 1
+    resize!
   end
 
   # O(n): has to shift over all the elements.
   def shift
     raise("index out of bounds") if self.length == 0
+    self.store.shift
+    self.length -= 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    self.store.unshift(val)
+    self.length += 1
+    resize!
   end
 
   protected
@@ -46,5 +61,8 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    if self.length > self.capacity
+      self.capacity *= 2
+    end
   end
 end
